2612d5269469bf4024720cba7df5cce07181e085
reim-weib-hr <hrrmanager985@gmail.com>
2024-03-06 08:00:21 -0500

    swapped to oone drive

--- src/index.js
+++ src/index.js
@@ -1,24 +1,17 @@
-import React, { Suspense } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
-// import Default from './App';
-// import { Transfer } from './Transfer';
 
-import Preloader from './Preloader/Preloader';
-// import FastSpin from './fastspin/'
-
-
-const LazyLoadedComp = React.lazy(()=> import('./Transfer'));
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-
+const root = ReactDOM.createRoot(document.getElementById('php'));
 root.render(
   <React.StrictMode>
-    <Suspense fallback={<Preloader /> }>
-      <LazyLoadedComp />
-    </Suspense>
+    <App />
   </React.StrictMode>
 );
 
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
